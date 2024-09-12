173fc8bebf503b5c6eef3dfbf121d7cb1e10aa91
EliCST <enerona@csumb.edu>
2024-09-11 17:18:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hi there");
+
+//Task 2
+let box2 = document.querySelector("#box2");
+console.log(box2); 
+box2.textContent = "Hi there";
+
+//Task 1
+let box1 = document.querySelector("#box1");
+console.log(box1);
+box1.style.border= "thick solid  #FFC0CB" ;
+
+//Task 3
+let box3 = document.querySelector("#box3");
+console.log(box3);
+box3.style.backgroundColor= "Green" ;
+
+//Task 5
+let box5 = document.querySelector("#box5");
+console.log(box5);
+box5.style.fontSize= "15px" ;
+
+//Task 4
+let box4 = document.querySelector("#box4");
+console.log(box4);
+box4.style.color= "#CBC3E3" ;
+
+//Task 6
+let boxImage = document.querySelector("#boxImage");
+console.log(boxImage);
+boxImage.src = "image2.png"
